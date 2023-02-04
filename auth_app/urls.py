a624edf744a14d06c7a98dfb8375de80f6b7a3a8
webdeveloper510 <developerweb6@gmail.com>
2023-02-04 18:35:40 +0530

    Update urls.py

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from auth_app.views import *
+from auth_app import views
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(),name='register'),
     path('login/', UserLoginView.as_view(),name='login'),
+    path('send-verification-email/', SendVerificationEmail.as_view(),name='send verification email'),
+
+    
 ]
